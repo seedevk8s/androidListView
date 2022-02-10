fe2e9582ed2971535fddf6633a231a4212ce434b
seedevk8s <seedevk8s@gmail.com>
2022-02-10 23:46:33 +0900

    chap08 어탭터뷰 - 리스트뷰 구현

---
+++ app/src/main/java/kr/co/listview/MainActivity.java
@@ -0,0 +1,66 @@
+package kr.co.listview;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.app.ListActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.Toast;
+
+public class MainActivity extends ListActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        String[] values = { "Apple", "Apricot", "Avocado", "Banana", "Blackberry",
+                            "Blueberry", "Cherry", "Coconut", "Cranberry",
+                            "Grape Raisin", "Honeydew", "Jackfruit", "Lemon", "Lime",
+                            "Mango", "Watermelon",
+                            "Apple", "Apricot", "Avocado", "Banana", "Blackberry",
+                            "Blueberry", "Cherry", "Coconut", "Cranberry",
+                            "Grape Raisin", "Honeydew", "Jackfruit", "Lemon", "Lime",
+                            "Mango", "Watermelon"};
+
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, values);
+
+        setListAdapter(adapter);
+    }
+
+    @Override
+    protected void onListItemClick(ListView l, View v, int position, long id) {
+        super.onListItemClick(l, v, position, id);
+
+        String item = (String) getListAdapter().getItem(position);
+        Toast.makeText(this, item + " selected", Toast.LENGTH_LONG).show();
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
